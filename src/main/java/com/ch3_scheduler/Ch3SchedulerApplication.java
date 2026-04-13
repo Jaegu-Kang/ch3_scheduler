a4c913ae13f998c0eb463dee27c9fc09b8d9bcfe
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-13 14:39:26 +0900

    Initial commit

---
+++ src/main/java/com/ch3_scheduler/Ch3SchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.ch3_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3SchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3SchedulerApplication.class, args);
+    }
+
+}
