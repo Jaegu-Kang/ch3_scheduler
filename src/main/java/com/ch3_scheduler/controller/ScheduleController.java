67c8a845543a0b495b0a0d1ba650e232f96b7c54
Jaegu-Kang <rkdworn8@gmail.com>
2026-04-13 17:57:36 +0900

    Lv1 일정 생성 작성

--- src/main/java/com/ch3_scheduler/controller/ScheduleController.java
+++ src/main/java/com/ch3_scheduler/controller/ScheduleController.java
@@ -1,12 +1,21 @@
 package com.ch3_scheduler.controller;
 
+import com.ch3_scheduler.dto.CreateScheduleRequest;
+import com.ch3_scheduler.dto.CreateScheduleResponse;
 import com.ch3_scheduler.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+
+    @PostMapping("/schedules")
+    public CreateScheduleResponse createSchedule(@RequestBody CreateScheduleRequest request){
+        return scheduleService.save(request);
+    }
 }
